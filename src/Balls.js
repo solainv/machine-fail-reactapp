4a8b76e858d15a554c75b7876388d280806ce6f8
solainv <146381104+solainv@users.noreply.github.com>
2024-05-07 11:36:25 +0200

    Add files via upload

---
+++ src/Balls.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Balls.css'; // Stile für die Animation
+
+function Balls() {
+  return (
+    <div className="container-ball">
+      <div className="ball first"></div>
+      <div className="ball second"></div>
+      <div className="ball third"></div>
+    </div>
+  );
+}
+
+export default Balls;
+
