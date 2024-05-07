1a9c637e30225f8f74fbd4193d582e0cf2305bd6
solainv <146381104+solainv@users.noreply.github.com>
2024-05-07 13:23:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,9 +54,6 @@ function App() {
                         ⚠ "Bitte haben Sie Verständnis dafür, dass ich nur die kostenlose Version des Render-Cloud-Servers 
                         nutze. Das bedeutet, dass das Dashboard, die Anfragen und Antworten eine Weile dauern können, 
                         insbesondere beim ersten Abschicken. Vielen Dank für Ihre Geduld."
-                     // </div>
-                      //<a href='/admin'>Administration</a>
-                    //</div>
 
                     <p>
                       Guten Tag! In diesem App  können Sie die potentielle Ausfallsituation Ihrer Maschine vorhersagen. 
